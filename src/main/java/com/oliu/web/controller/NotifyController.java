7e486cfd0969945c66b66acf447b6227b2dc7499
oliu <lifen.zhuo@gmail.com>
2017-03-07 15:05:08 +0800

    add

--- src/main/java/com/oliu/web/controller/NotifyController.java
+++ src/main/java/com/oliu/web/controller/NotifyController.java
@@ -1,7 +1,19 @@
 package com.oliu.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by zli on 2017/3/6.
  */
+@Controller
+@RequestMapping("/n")
 public class NotifyController {
+    @RequestMapping(value = "/getNotify",method = RequestMethod.GET)
+    @ResponseBody
+    public Object getNotify(String str){
+        return "hello";
+    }
 }
