a289c2fa945490a52676d35147d8da40978f13f5
oliu <lifen.zhuo@gmail.com>
2017-08-23 14:45:56 +0800

    add test get property

--- src/main/java/com/oliu/web/controller/HelloController.java
+++ src/main/java/com/oliu/web/controller/HelloController.java
@@ -3,10 +3,7 @@ package com.oliu.web.controller;
 import com.oliu.config.GirlProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by zli on 2017/3/2.
@@ -20,12 +17,14 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
+    @Value("${girl.age}")
+    private Integer age;
+
     /**
-     *
      * @return
      */
-    @RequestMapping(value = "/getPropertiesValue",method = RequestMethod.GET)
-    public String say(){
+    @RequestMapping(value = "/getPropertiesValue", method = RequestMethod.GET)
+    public String say() {
         StringBuffer sb = new StringBuffer();
         sb.append("Try get properties value.");
         sb.append("The projectName is:");
@@ -37,6 +36,12 @@ public class HelloController {
         return sb.toString();
     }
 
+    @GetMapping(value = "/getAge")
+    private String getAge() {
+        String ageStr = age.toString();
+        return ageStr;
+    }
+
     /**
      * test two url one interface
      * http://localhost:8080/one?id=1
@@ -44,8 +49,8 @@ public class HelloController {
      * @param id
      * @return
      */
-    @RequestMapping(value = {"/one","/two"},method = RequestMethod.GET)
-    public String one(@RequestParam(value = "id",required = false,defaultValue = "0") String id){
+    @RequestMapping(value = {"/one", "/two"}, method = RequestMethod.GET)
+    public String one(@RequestParam(value = "id", required = false, defaultValue = "0") String id) {
         return "the api one or two id value is:" + id;
     }
 }
